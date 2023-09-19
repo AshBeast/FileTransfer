59eefed22ac85fd52113108889a490f87f71caec
Ashkan Zahedanaraki <ashkanzahde@gmail.com>
2023-09-19 08:48:55 -0700

    pushing main content

---
+++ saved/client.py
@@ -0,0 +1,65 @@
+import socket
+import sys
+
+SOCKET_FILE = './domain_socket.sock'
+
+def signal_handler(sig, frame):
+    print('stopping client')
+    client.close()
+    sys.exit(0)
+
+def serverCheck():
+    try:
+        client.connect(SOCKET_FILE)
+    except socket.error:
+        print("Server is not running.")
+        sys.exit(1)
+
+
+client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+# Ensure the server is running
+serverCheck()
+
+# Check for file arguments
+filenames = []
+if len(sys.argv) < 2:
+    print("Please provide one or more file names as arguments.")
+    filenames = input("Please provide one or more file names: ").split()
+else:
+    filenames = sys.argv[1:]
+
+# Read and send file contents
+for filename in filenames:
+    try:
+        with open(filename, 'r') as f:
+            contents = f.read()
+            
+            # Send filename
+            client.sendall((filename + "\n").encode('utf-8'))
+            print(client.recv(1024))
+
+            # Send file size
+            client.sendall((str(len(contents)) + "\n").encode('utf-8'))
+            print(client.recv(1024))
+            
+            # Send file contents
+            client.sendall(contents.encode('utf-8'))
+            print(f"Sent contents of {filename} to server.")
+            response = client.recv(1024)
+            print("Received:", response.decode('utf-8'))
+            
+    except FileNotFoundError:
+        print(f"File {filename} not found.")
+        continue
+    except Exception as e:
+        print(f"Error reading {filename}: {str(e)}")
+        break
+
+client.sendall("/Done".encode('utf-8'))
+client.close()
+
+
+
+
+
---
+++ saved/server.py
@@ -0,0 +1,72 @@
+import socket
+import os
+import sys
+import signal
+
+SOCKET_FILE = './domain_socket.sock'
+
+class File:
+    def __init__(self, name, content):
+        self.name = name
+        self.content = content
+
+    def save(self, directory="saved"):
+        # Ensure the directory exists, create if it doesn't
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        # Write the content to the file
+        with open(os.path.join(directory, self.name), 'w') as f:
+            f.write(self.content)
+
+
+# Handle SIGINT
+def signal_handler(sig, frame):
+    print('\nShutting Down Server')
+    clearSocket()
+    server.close()
+    sys.exit(0)
+
+# Ensure socket file does not already exist
+def clearSocket():
+    if os.path.exists(SOCKET_FILE):
+        os.remove(SOCKET_FILE)
+
+# Register the signal_handler for SIGINT
+signal.signal(signal.SIGINT, signal_handler)
+
+clearSocket()
+
+# Create a UNIX socket
+server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+server.bind(SOCKET_FILE)
+server.listen(1)
+
+while True:
+    print("Server waiting for connection...")
+    conn, addr = server.accept()
+    print("Connected.")
+
+    while True: 
+        # Receive filename
+        filename = conn.recv(1024).decode('utf-8').strip()
+        if (filename == "/Done"):
+            break
+
+        print(f"Receiving data for file: {filename}")
+        conn.sendall(b"server has file name")
+        
+        # Receive file size
+        file_size = int(conn.recv(1024).decode('utf-8').strip())
+        print(f"Expected file size: {file_size} bytes")
+        conn.sendall(b"server has file size")
+
+        # Receive file contents and saving the file
+        data = conn.recv(file_size).decode('utf-8')
+        print("Received File Contents:\n", data)
+        my_file = File(filename, data)
+        my_file.save()
+        conn.sendall(b"Files received by server! and saved")
+
+    
+    conn.close()
